23a6f73ea0cee525436cc36850dbebacc3ae219b
Charley <56961474+charleyr0@users.noreply.github.com>
2025-07-26 12:22:12 +0100

    rearrange openOnScreenWindow.m

--- openOnscreenWindow.m
+++ openOnscreenWindow.m
@@ -1,45 +1,33 @@
 function [screen] = openOnscreenWindow(ex)
 
-    % this function expects some variables in ex -
-    %
+    % this function expects these variables in ex:
     % ex.display.screenRect
-    %  e.g. [0 0 1920 1080] for fullscreen
-    %
     % ex.display.backgroundColour
-    %  e.g. screen.colours.grey
-    %
     % ex.display.textSize 
     % ex.display.textFont
-    %  for setting the default text settings for new the window this opens
-    
-    % setup the display
-    screen.scrNum = max(Screen('Screens'));  % displays always on the primary monitor I think
-
-    % sync stuff
-    Screen('Preference','SyncTestSettings', 0.01, 40, 0.2, 5); % sets more forgiving test parameters which removes the flashing warning triangle - see psychtoolbox.org/docs/SyncTrouble
-    Screen('Preference', 'SkipSyncTests', ex.debug); % skip sync tests iff debug mode
-    
+       
     % open a window, get the width and height
     [screen.window, screen.windowRect] = PsychImaging('OpenWindow', screen.scrNum, ex.display.backgroundColour, ex.display.screenRect); % TODO do we even need to keep windowRect in screen?
     screen.width = screen.windowRect(3);
     screen.height = screen.windowRect(4);
-    
-    % Enable alpha blending for anti-aliasing (motion smoothing when drawn objects move around)
-    Screen('BlendFunction', screen.window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
-    
-    % get the frame duration. not sure this is used anywhere - print as a check, or remove it?
-    screen.ifi = Screen('GetFlipInterval', screen.window);  
+    screen.scrNum = max(Screen('Screens'));  % this makes it displays always on the primary monitor I think
+    screen.ifi = Screen('GetFlipInterval', screen.window);  % get the frame duration. not sure this is used anywhere - print as a check, or remove it?
+    screen.frameRate = Screen('NominalFrameRate', screen.window);  % "Datapixx frame rate -- decide how to not mix up with PC screen FR"
 
-    % "Datapixx frame rate -- decide how to not mix up with PC screen FR"
-    screen.frameRate = Screen('NominalFrameRate', screen.window);  
-    
-    % set to the maximum possible priority level (??)
-    topPriorityLevel = MaxPriority(screen.window);  
-    Priority(topPriorityLevel);
-    
+    % sync stuff
+    Screen('Preference','SyncTestSettings', 0.01, 40, 0.2, 5); % sets more forgiving test parameters which removes the flashing warning triangle - see psychtoolbox.org/docs/SyncTrouble
+    Screen('Preference', 'SkipSyncTests', ex.debug); % skip sync tests iff debug mode
+
     % set the default text size and font
     Screen('TextSize', screen.window, ex.display.textSize);
     Screen('TextFont', screen.window, ex.display.textFont);
+
+    % set to the maximum possible priority level (??)
+    topPriorityLevel = MaxPriority(screen.window);  
+    Priority(topPriorityLevel);
+
+    % Enable alpha blending for anti-aliasing (motion smoothing when drawn objects move around)
+    Screen('BlendFunction', screen.window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
     
     % so that keypresses (e.g. ttttttttttt from the scanner) will not show in command window
     ListenChar(2);
