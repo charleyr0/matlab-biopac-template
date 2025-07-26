bbb8371aff3c4d1e1d8c571d7287c8040f56f66c
Charley <56961474+charleyr0@users.noreply.github.com>
2025-07-26 11:40:52 +0100

    remove sca

--- biopac/biopacStartAcquisition.m
+++ biopac/biopacStartAcquisition.m
@@ -3,14 +3,12 @@ function startTime = biopacStartAcquisition()
     % attempt to setup for acquisition start
     msg = calllib('mpdev', 'startMPAcqDaemon');
     if ~strcmp(msg, 'MPSUCCESS')
-        sca;
         error('Failed to start dynamometer acquisition daemon: %s', msg);
     end
 
     % attempt to start acquisition
     msg = calllib('mpdev', 'startAcquisition');
     if ~strcmp(msg, 'MPSUCCESS')
-        sca; 
         calllib('mpdev', 'disconnectMPDev');
         error('Failed to start dynamometer acquisition: %s', msg);
     end
